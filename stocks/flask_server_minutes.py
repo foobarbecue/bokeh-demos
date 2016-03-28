144d70fe4ce8562185263c727c40b9ef9601eb47
Mike Sarahan <msarahan@gmail.com>
2016-03-28 10:06:07 -0500

    Copy crossdomain function from bokeh
    from bokeh/examples/howto/ajax_source.py.  bokeh.server.crossdomain seems like it moved there.

--- stocks/flask_server_minutes.py
+++ stocks/flask_server_minutes.py
@@ -10,13 +10,65 @@ import numpy as np
 import pandas as pd
 import dask.array as da
 from subsample import coarsen
-from bokeh.server.crossdomain import crossdomain
 from StringIO import StringIO
 
 FACTOR_BASE = 15000
 fromtimestamp = dt.datetime.fromtimestamp
 
 app = Flask(__name__)
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    """
+    Decorator to set crossdomain configuration on a Flask view
+    For more details about it refer to:
+    http://flask.pocoo.org/snippets/56/
+    """
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+
+    if headers is not None and not isinstance(headers, string_types):
+        headers = ', '.join(x.upper() for x in headers)
+
+    if not isinstance(origin, string_types):
+        origin = ', '.join(origin)
+
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        @wraps(f)
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            requested_headers = request.headers.get(
+                'Access-Control-Request-Headers'
+            )
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            elif requested_headers:
+                h['Access-Control-Allow-Headers'] = requested_headers
+            return resp
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+
+    return decorator
 
 
 def to_seconds(ts):
